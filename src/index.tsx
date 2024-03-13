03d739c4c58d9a2e196520aea97bb6e9c21b0736
kanchika-kapoor <kanchika.ai@gmail.com>
2024-03-14 02:06:25 +0530

    integrated data with components

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://main--spacex-l4uc6p.apollographos.net/graphql',
+  cache: new InMemoryCache()
+})
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
